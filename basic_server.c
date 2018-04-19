b44e9ae7d755a262a978a1a34e00e531710730b0
kiwi <qiweizhang8523@gmail.com>
2018-04-20 01:11:19 +0800

    finish basic server

--- basic_server.c
+++ basic_server.c
@@ -7,6 +7,18 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <errno.h> 
+#include <stdlib.h>
+#include <stdio.h> 
+//#include <iostream>
+
+#include <errno.h> 
+#include <string.h> 
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_nota()
 //服务器端
 
 #define port_number 3333
@@ -28,7 +40,56 @@ int sock_get(){
 	/***************************init*************************************/
 	bzero(&server_addr,sizeof(struct sockaddr_in));
 	server_addr.sin_family=AF_INET;
-	server_addr.sin_addr.sin_addr=;
+	server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	server_addr.sin_port=htons(port_number);
 
+
+	/***************************bind*************************************/
+	if(bind(sockfd,(struct sockaddr*)(&server_addr),sizeof(struct sockaddr))==-1){
+		printf("bind error is %s\n\a", strerror(errno));
+		fprintf(stderr, "bind error is %s\n\a", strerror(errno));
+		exit(1);
+	}
+
+	/***************************listen*************************************/
+	if(listen(sockfd,5)==-1){
+		printf("listen error is %s\n\a",strerror(errno));
+		fprintf(stderr, "listen error is %s\n\a", strerror(errno));
+		exit(1);
+	}
+
+	/***************************listen to client*************************************/
+	int count=0;
+	while(1){
+		count++;
+		printf("begin listen %d times\n", count);
+		sin_size=sizeof(struct sockaddr_in);
+
+		if((new_fd=accept(sockfd,(struct sockaddr*)&client_addr,&sin_size))==-1){
+			printf("connect error is %s\n\a", strerror(errno));
+			fprintf(stderr, "connect error is %s\n\a", strerror(errno));
+			exit(1);
+		}
+	
+		printf("server get connect to client: %s\n\a", inet_ntoa(client_addr.sin_addr));
+		fprintf(stderr, "server get connetion from %s\n\a", inet_ntoa(client_addr.sin_addr));
+	
+		if(write(new_fd,hello,strlen(hello))==-1){
+			printf("write errno is %s\n\a", strerror(errno));
+			fprintf(stderr, "write error is %s\n\a", strerror(errno));
+			exit(1);
+		}
+		close(new_fd);
+	}
+	close(sockfd);
+	exit(0);
+	return 1;
 }
 
+
+
+int main(int argc, char const *argv[])
+{
+	sock_get();
+	return 0;
+}
